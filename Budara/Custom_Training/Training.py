5c5bab7a7291be9f00112d65d0671dba6831136b
ASUS <w1698407@my.westminster.ac.uk>
2019-04-03 10:43:34 +0530

    crib defining
    Signed-off-by: ASUS <w1698407@my.westminster.ac.uk>

--- Budara/Custom_Training/Training.py
+++ Budara/Custom_Training/Training.py
@@ -23,3 +23,34 @@ label_name = column_names[-1]
 
 print("Features: {}".format(feature_names))
 print("Label: {}".format(label_name))
+
+class_names = ['Iris setosa','Iris versicolor','Iris virginica']
+
+batch_size = 32
+train_dataset = tf.contrib.data.make_csv_dataset(
+    train_dataset_fp,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1
+)
+features, labels = next(iter(train_dataset))
+
+features
+plt.scatter(features['petal_length'].numpy(),
+            features['sepal_length'].numpy(),
+            c=labels.numpy(),
+            cmap='viridis')
+
+plt.xlabel("Petal length")
+plt.ylabel("Sepal length")
+print("done")
+def pack_features_vector(features, labels):
+  """Pack the features into a single array."""
+  features = tf.stack(list(features.values()), axis=1)
+  return features, labels
+
+train_dataset = train_dataset.map(pack_features_vector)
+features, labels = next(iter(train_dataset))
+
+print(features[:5])
