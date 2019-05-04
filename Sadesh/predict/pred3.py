5d7ea24736530379fcd1a1ec5fa3f44a0f059415
sadesh97 <w1698452@my.westminster.ac.uk>
2019-05-04 23:26:32 +0530

    machine_learning

---
+++ Sadesh/predict/pred3.py
@@ -0,0 +1,70 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+modelfile = '/home/sadesh manranju/Desktop/finalizedModel.sav'
+datafile='/home/sadesh manranju/Desktop/testBank.csv'
+savedfile="/home/sadesh manranju/Desktop/prediction.txt"
+
+loaded_model = pickle.load(open(modelfile , 'rb'))
+cols=['Ammount','Period','EMI','purpose','secutityType','CRIB','entity','POS','assets','installments','tax']
+df = pd.read_csv(datafile, sep=",", header=None, names=cols)
+
+
+ammount=df['Ammount']
+period=df['Period']
+emi=df['EMI']                                    
+POL=df['purpose']
+newPOL=None
+security=df['secutityType']
+newSecurity=None
+CRIB=df['CRIB']
+newCRIB=None
+TOE=df['entity']
+newTOE=None
+POS=df['POS']
+assets=df['assets']
+loanI=df['installments']
+tax=df['tax']
+newTax=None
+
+sample=POL[0]
+if(sample=="Purchase of Machinery & Equipment"):
+    newPOL=0
+elif(sample=="Purchase") or (sample=="Purchase ") :
+    newPOL=1
+elif(sample=="Business & Redemption"):
+    newPOL=2
+elif(sample=="Education"):
+    newPOL=3
+elif(sample=="Renovation/Repair(Improvement)"):
+    newPOL=4
+elif(sample=="Redemption"):
+    newPOL=5
+elif(sample=="Business"):
+    newPOL=6
+elif(sample=="Purchase Of House"):
+    newPOL=7
+elif(sample=="OTHER"):
+    newPOL=8
+elif(sample=="Transport & Storage"):
+    newPOL=9
+elif(sample=="Redemption & Construction"):
+    newPOL=10
+elif(sample=="Construction"):
+    newPOL=11
+elif(sample=="Purchasing Of Building Block"):
+    newPOL=12
+elif(sample=="Business Use only"):
+    newPOL=13
+elif(sample=="Expansion of Business"):
+    newPOL=14
+elif(sample=="Agriculture/Farming"):
+    newPOL=15
+elif(sample=="Furnitures"):
+    newPOL=16
+elif(sample=="Redemption & Furniture"):
+    newPOL=17
+elif(sample=="Dairy and Poultry Products"):
+    newPOL=18
+
