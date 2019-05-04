b4cd7b173e6b984b287a99c7c1c48ab7d2dfe471
HarendraRanasinghe95 <w1699674@my.westminster.ac.uk>
2019-05-04 11:01:20 +0530

    add funDebtServiceCovergae

--- Harendra/Calculations/ScoreCalculations.py
+++ Harendra/Calculations/ScoreCalculations.py
@@ -3,6 +3,7 @@ def __init__(self, name, crib, ):
     self.crib = crib
 
 
+
 def funtionCribScore(crib):
     if (crib == 0):
         cribScore = 150
@@ -11,4 +12,16 @@ def funtionCribScore(crib):
     elif (crib < 60):
         cribScore = 75
     else:
-        cribScore = 0
+        cribScore=0
+
+def funDebtServiceCovergae(debt):
+            if (debt < 1.00):
+                debtScore = 20
+            elif (debt < 1.50):
+                debtScore = 50
+            elif (debt < 2.00):
+                debtScore = 75
+            elif (debt < 2.50):
+                debtScore = 100
+            elif (debt < 100):
+                debtScore = 100
