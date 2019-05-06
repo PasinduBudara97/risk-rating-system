0340ef98a127758b27490c726d7a9ceac5fabb40
sadesh97 <w1698452@my.westminster.ac.uk>
2019-05-06 07:38:12 +0530

    machine_learning

--- Sadesh/predict/algo_choose.py
+++ Sadesh/predict/algo_choose.py
@@ -16,3 +16,18 @@ for k in range(len(getData[0])):
         tot=float(tot)+x[j]
     x=[int(getData[0][i])/tot,int(getData[1][i])/tot,int(getData[2][i])/tot,int(getData[3][k])/tot,int(getData[4][k])/tot,int(getData[5][k])/tot,int(getData[6][k])/tot,int(getData[7][i])/tot,int(getData[8][i])/tot,int(getData[9][i])/tot,int(getData[10][k])/tot,'Notok']
     list1.append(x)
+
+	
+
+df=pd.read_excel('/home/Desktop/info.xlsx',index_col=0)
+ammount=df['Loan amount']
+period=df['Period']
+emi=df['EMI']                                    
+POL=df['Purpose of loan']
+newPOL=[]
+security=df['Security type']
+newSecurity=[]
+CRIB=df['CRIB status']
+newCRIB=[]
+TOE=df['type of entity']
+newTOE=[]
