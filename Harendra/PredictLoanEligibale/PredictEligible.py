1888ce9e400fd3d25da65d2b5fd4131952f2a8a5
HarendraRanasinghe95 <w1699674@my.westminster.ac.uk>
2019-05-06 06:41:31 +0530

    predicting Loan Eligible2

--- Harendra/PredictLoanEligibale/PredictEligible.py
+++ Harendra/PredictLoanEligibale/PredictEligible.py
@@ -4,4 +4,27 @@ import numpy as np
 
 modelfile = 'C:\Users\Sadesh Manaranju\Desktop\finalizedModel.sav'
 datafile='C:\Users\Sadesh Manaranju\Desktop\testBank.csv'
-savedfile="/home/Desktop/prediction.txt"
+savedfile="/home/Desktop/prediction.txt"ammount=df['Ammount']
+period=df['Period']
+emi=df['EMI']
+POL=df['purpose']
+newPOL=None
+security=df['secutityType']
+newSecurity=None
+CRIB=df['CRIB']
+newCRIB=None
+TOE=df['entity']
+newTOE=None
+POS=df['POS']
+assets=df['assets']
+loanI=df['installments']
+tax=df['tax']
+newTax=None
+
+def PreProcessData(getData):
+
+    sum=0
+    for i in range(len(getData)):
+        sum=sum+getData[i]
+    tList=(ammount[0]/sum,period[0]/sum,emi[0]/sum,newPOL/sum,newSecurity/sum,newCRIB/sum,newTOE/sum,POS[0]/sum,assets[0]/sum,loanI[0]/sum,newTax/sum)
+    return tList
