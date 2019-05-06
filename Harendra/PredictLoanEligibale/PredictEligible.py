9e63fac0f5b511d154b0a664b42196f37ea9b181
HarendraRanasinghe95 <w1699674@my.westminster.ac.uk>
2019-05-06 06:15:32 +0530

    predicting Loan Eligible

---
+++ Harendra/PredictLoanEligibale/PredictEligible.py
@@ -0,0 +1,7 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+modelfile = 'C:\Users\Sadesh Manaranju\Desktop\finalizedModel.sav'
+datafile='C:\Users\Sadesh Manaranju\Desktop\testBank.csv'
+savedfile="/home/Desktop/prediction.txt"
