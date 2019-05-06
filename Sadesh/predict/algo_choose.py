3320c0b317dcbc2db1d8f8e06937f4c91caadfb5
sadesh97 <w1698452@my.westminster.ac.uk>
2019-05-06 07:53:14 +0530

    machine_learning

--- Sadesh/predict/algo_choose.py
+++ Sadesh/predict/algo_choose.py
@@ -53,3 +53,12 @@ for k in range(1476):
     x=[int(getData[0][i]),int(getData[1][i]),int(getData[2][i]),int(getData[3][k]),int(getData[4][k]),int(getData[5][k]),int(getData[6][k]),int(getData[7][i]),int(getData[8][i]),int(getData[9][i]),int(getData[10][k])]
     if(k==0):
         print(x)
+		
+		
+  
+    tot=0
+    for j in range(len(x)):
+        tot=float(tot)+x[j]
+    x=[int(getData[0][i])/tot,int(getData[1][i])/tot,int(getData[2][i])/tot,int(getData[3][k])/tot,int(getData[4][k])/tot,int(getData[5][k])/tot,int(getData[6][k])/tot,int(getData[7][i])/tot,int(getData[8][i])/tot,int(getData[9][i])/tot,int(getData[10][k])/tot,'ok']
+    list2.append(x)
+
